e758495a36a215cd720e9c2a96d0e594bf4730c5
Beiko14 <heiko.hollerith14@gmx.de>
2021-06-23 12:44:19 +0200

    created the Tetrominos forms as arrays

--- app.js
+++ app.js
@@ -14,4 +14,57 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(squares);
 
 
+    //      0 2   1 2   2 2
+    //
+    //      0 1   1 1   2 1
+    //
+    //      0 0   1 0   2 0
+    //
+    //      width is x-axis
+    //
+    // Tetrominoes
+    const lTetromino = [
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [0, width, width + 1, width + 2],
+        [1, 0, width, width * 2]
+    ]
+
+    const zTetromino = [
+        [0, width, width + 1, width * 2 + 1],
+        [2, 1, width + 1, width],
+        [0, width, width + 1, width * 2 + 1],
+        [2, 1, width + 1, width]
+    ]
+
+    const tTetromino = [
+        [0, width, width * 2, width + 1],
+        [0, 1, 2, width + 1],
+        [1, width + 1, width * 2 + 1, width],
+        [1, width, width + 1, width + 2]
+    ]
+
+    const qTetromino = [
+        [0, width, 1, width + 1],
+        [0, width, 1, width + 1],
+        [0, width, 1, width + 1],
+        [0, width, 1, width + 1]
+    ]
+
+    const iTetromino = [
+        [0, 1, 2, 3],
+        [0, width, width * 2, width * 3],
+        [0, 1, 2, 3],
+        [0, width, width * 2, width * 3]
+    ]
+
+
+
+
+
+
+
+
+
+
 })
